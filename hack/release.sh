8e4f3706017e0972b9dff071c07bf86f265c0204
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-06-06 15:56:20 -0300

    feat: test script and improve release script

--- hack/release.sh
+++ hack/release.sh
@@ -3,6 +3,15 @@ set -e
 helm init --client-only
 helm lint thanos
 mkdir -p pkg
+
+oldProj="$(gcloud config get-value project)"
+trap 'gcloud config set project $oldProj' EXIT
+gcloud config set project carlos-kube
+
+oldAccount="$(gcloud config get-value account)"
+trap 'gcloud config set account $oldAccount' EXIT
+gcloud config set account carlos@carlosbecker.com
+
 gsutil rsync gs://carlos-charts pkg
 helm package thanos --destination pkg
 helm repo index pkg/ --url "https://carlos-charts.storage.googleapis.com"
---
+++ hack/test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+trap 'k3d delete -n thanos-test' EXIT
+
+k3d create -n thanos-test -w
+export KUBECONFIG="$(k3d get-kubeconfig --name='thanos-test')"
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller --upgrade --wait
+
+helm install --namespace thanos --name thanos ./thanos \
+	--wait \
+	--timeout 600 \
+	-f values.yaml \
+	--set-file objectStore=thanos-storage-config.yaml
+
