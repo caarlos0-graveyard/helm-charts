f82820571ac813d57746e8df9d6a9be3b3a992e0
Carlos Alexandro Becker <caarlos0@gmail.com>
2019-06-25 13:03:30 -0300

    feat: store ingress grpc
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -4,13 +4,8 @@ helm init --client-only
 helm lint thanos
 mkdir -p pkg
 
-oldProj="$(gcloud config get-value project)"
-trap 'gcloud config set project $oldProj' EXIT
-gcloud config set project carlos-kube
-
-oldAccount="$(gcloud config get-value account)"
-trap 'gcloud config set account $oldAccount' EXIT
 gcloud config set account carlos@carlosbecker.com
+gcloud config set project carlos-kube
 
 gsutil rsync gs://carlos-charts pkg
 helm package thanos --destination pkg
