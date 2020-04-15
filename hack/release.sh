a9bcb0de538fe85442e72c8515ebf78639a30654
Carlos Alexandro Becker <caarlos0@gmail.com>
2020-04-15 10:51:28 -0300

    fix: index no cache
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -11,5 +11,6 @@ gsutil rsync gs://carlos-charts pkg
 helm package thanos --destination pkg
 helm repo index pkg/ --url "https://carlos-charts.storage.googleapis.com"
 gsutil rsync pkg gs://carlos-charts
+gsutil setmeta -h "Cache-Control:private,max-age=0,no-transform" gs://carlos-charts/index.yaml
 
 helm repo update
